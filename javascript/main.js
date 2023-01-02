c1e49570687021e119648fa92e1fecc88d10d60d
pagidik <pagidi.k@northeastern.edu>
2023-01-02 12:42:25 -0800

    Initial Commit

--- javascript/main.js
+++
@@ -1,9 +0,0 @@
-function fixVideoWidth() {
-    var width = $("#vid1").parent().width();
-    var height = width/1.7777;
-    console.log(width);
-    console.log(height);
-    $("#vid1").width(width);
-    $("#vid1").height(height);
-};
-fixVideoWidth();
